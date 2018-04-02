a9ce62dc73e78eeac015310f9cf488eed56c7994
oumarucho <sissoko.oumarou1991@mail.com>
2018-04-02 14:07:45 +0200

    possibility to load the current controller

---
+++ app/controllers/Pages.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: oumarucho
+ * Date: 02/04/2018
+ * Time: 14:05
+ */
+
+class Pages
+{
+    public function __construct()
+    {
+        echo 'Pages Loaded';
+    }
+
+}
--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -12,11 +12,34 @@
 
     public function __construct()
     {
-        $this->getUrl();
+        //print_r($this->getUrl());
+        $url = $this->getUrl();
+
+        //Look in controllers for the first value
+        $url = '../app/controllers/'.ucwords($url[0]).'.php';
+        if(file_exists('../app/controllers/'.ucwords($url[0]).'.php')){
+            //if exists set as controller
+            $this->currentController = ucwords($url[0]);
+            //unset 0 index
+            unset($url[0]);
+        }
+
+        //Require the controller
+        require_once '../app/controllers/'.$this->currentController.'.php';
+
+        //Instantiate controller class
+        $this->currentController = new $this->currentController;
     }
 
     public function getUrl(){
-        echo $_GET['url'];
+        if(isset($_GET['url'])){
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+            return $url;
+        }
+
+        return [];
     }
 
     }
